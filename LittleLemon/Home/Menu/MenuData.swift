02de3fb57c4fb4b935899a4bae0715fd995d6117
Abdelrahman <88583085+abdel-17@users.noreply.github.com>
2023-03-30 21:14:10 +0200

    Database is now cleared after fetching
    This prevents duplicate items

--- LittleLemon/Home/Menu/MenuData.swift
+++ LittleLemon/Home/Menu/MenuData.swift
@@ -5,39 +5,40 @@
 //  Created by Abdel on 29/03/2023.
 //
 
-import Foundation
 import CoreData
+import Foundation
 
 struct Menu: Codable {
     let items: [MenuItem]
-    
+
     enum CodingKeys: String, CodingKey {
         case items = "menu"
     }
-    
+
     /// Fetches menu data in JSON format from the given url.
     static func json(url: URL) async throws -> Menu {
-        debugPrint("Fetching menu from \(url)")
         let (data, _) = try await URLSession.shared.data(from: url)
-        return try JSONDecoder().decode(Menu.self, from: data)
+        let menu = try JSONDecoder().decode(Menu.self, from: data)
+        debugPrint("Fetched menu from \(url)")
+        return menu
     }
 }
 
 struct MenuItem: Codable {
     let title: String
-    
+
     let description: String
-    
+
     let price: String
-    
+
     let image: String
-    
+
     let category: String
 }
 
 extension Dish {
     /// Saves the menu items as dishes to the given context.
-    static func create(from menu: Menu, context: NSManagedObjectContext) {
+    static func create(from menu: Menu, context: NSManagedObjectContext) throws {
         menu.items.forEach { item in
             let dish = Dish(context: context)
             dish.title = item.title
@@ -46,12 +47,25 @@ extension Dish {
             dish.image = item.image
             dish.category = item.category
         }
-        
-        do {
-            try context.save()
-        } catch {
-            debugPrint("Saving context changes failed: \(error.localizedDescription)")
-        }
+
+        try context.save()
+        debugPrint("Saved menu to CoreData")
+    }
+
+    static func deleteAll(context: NSManagedObjectContext) throws {
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: Dish.fetchRequest())
+        deleteRequest.resultType = .resultTypeObjectIDs
+
+        let batchDelete = try context.execute(deleteRequest) as? NSBatchDeleteResult
+        guard let deleteResult = batchDelete?.result as? [NSManagedObjectID] else { return }
+
+        let deletedObjects: [AnyHashable: Any] = [
+            NSDeletedObjectsKey: deleteResult,
+        ]
+        NSManagedObjectContext.mergeChanges(
+            fromRemoteContextSave: deletedObjects,
+            into: [context]
+        )
+        debugPrint("Deleted all dishes from CoreData")
     }
 }
-
